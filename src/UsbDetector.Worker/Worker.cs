e592799f5b5f09b82f2e892479fc308886c199ce
Alberto Viezzi <aviezzi@outlook.com>
2021-11-06 00:00:00 +0100

    Add insert watcher with start and stop management

--- src/UsbDetector.Worker/Worker.cs
+++ src/UsbDetector.Worker/Worker.cs
@@ -1,21 +1,29 @@
-﻿namespace UsbDetector.Worker;
+﻿using System.Management;
+
+namespace UsbDetector.Worker;
 
 public class Worker : IHostedService
 {
     private readonly ILogger<Worker> _logger;
 
+    private const string InsertQuery = @"SELECT * FROM __InstanceCreationEvent WITHIN 2 WHERE TargetInstance ISA 'Win32_USBHub'";
+    private readonly ManagementEventWatcher _insertWatcher = new(new WqlEventQuery(InsertQuery));
+
     public Worker(ILogger<Worker> logger)
     {
         _logger = logger;
+        _insertWatcher.EventArrived += (_, __) => Console.WriteLine("Usb Inserted");
     }
 
     public Task StartAsync(CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        _insertWatcher.Start();
+        return Task.CompletedTask;
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        _insertWatcher.Stop();
+        return Task.CompletedTask;
     }
 }
