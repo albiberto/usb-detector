34cdb62c34c7d66d49a8164413957ff53ce5755d
Alberto Viezzi <aviezzi@outlook.com>
2021-11-10 00:00:00 +0100

    Add usb detector class

---
+++ src/UsbDetector.Worker/Detectors/UsbDetector.cs
@@ -0,0 +1,21 @@
+﻿using System.Management;
+using UsbDetector.Worker.Abstract;
+
+namespace UsbDetector.Worker.Detectors;
+
+public class UsbDetector : IUsbDetector
+{
+    private const string SearchQuery = @"SELECT * FROM Win32_DiskDrive WHERE InterfaceType='USB'";
+
+    public void OnInserted(object sender, EventArrivedEventArgs e)
+    {
+        using var searcher = new ManagementObjectSearcher(SearchQuery);
+        foreach (var currentObject in searcher.Get())
+        {
+            var management = new ManagementObject("Win32_PhysicalMedia.Tag='" + currentObject["DeviceID"] + "'");
+            var serialNumber = $"{management["SerialNumber"]}";
+            
+            Console.WriteLine($"{serialNumber} USB inseted");
+        }
+    }
+}
