c92ab95b4158644d5d17b45df22f544a66242c28
Alberto Viezzi <aviezzi@outlook.com>
2021-11-19 00:00:00 +0100

    Add ConfigureWebHostDefault to add SignalR hub endpoint

--- src/UsbDetector.Worker/Program.cs
+++ src/UsbDetector.Worker/Program.cs
@@ -1,14 +1,23 @@
 ﻿using UsbDetector.Worker;
 using UsbDetector.Worker.Abstract;
+using UsbDetector.Worker.Hubs;
 using Detector = UsbDetector.Worker.Detectors.UsbDetector;
 
-IHost host = Host.CreateDefaultBuilder(args)
+var host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
         services.AddHostedService<Worker>();
         services.AddScoped<IUsbDetector, Detector>();
         services.AddSignalR();
     })
+    .ConfigureWebHostDefaults(webBuilder => webBuilder.Configure(app =>
+    {
+        app.UseRouting();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapHub<UsbHub>("/usbhub");
+        });
+    }))
     .Build();
 
 await host.RunAsync();
