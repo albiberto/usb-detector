c94a3ccda2c1c2b143387896fc419ec4e57cf7f3
Alberto Viezzi <aviezzi@outlook.com>
2021-11-25 00:00:00 +0100

    Add cors

--- src/UsbDetector.Worker/Program.cs
+++ src/UsbDetector.Worker/Program.cs
@@ -9,9 +9,17 @@ var host = Host.CreateDefaultBuilder(args)
         services.AddHostedService<Worker>();
         services.AddScoped<IUsbDetector, Detector>();
         services.AddSignalR();
+        services.AddCors();
     })
     .ConfigureWebHostDefaults(webBuilder => webBuilder.Configure(app =>
     {
+        app.UseCors(policy =>
+        {
+            policy.AllowAnyHeader();
+            policy.AllowAnyMethod();
+            policy.AllowAnyOrigin();
+        });
+        
         app.UseRouting();
         app.UseEndpoints(endpoints =>
         {
